aa762a76e0fecfb4d66759963f20fce342552a8f
TaishiU <nbadreamteam3@gmail.com>
2021-04-25 16:57:22 +0900

    fourth commit, create 'provider'

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,42 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:third/mainModel.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  final String text = 'Provider';
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Provider'),
-        ),
-        body: Center(
-          child: Column(
-            children: [
-              const SizedBox(height: 250),
-              Text(
-                text,
-                style: TextStyle(
-                  fontSize: 30,
-                ),
-              ),
-              RaisedButton(
-                child: Text('change'),
-                onPressed: () {},
-              ),
-            ],
+      home: ChangeNotifierProvider<mainModel>(
+        create: (_) => mainModel(),
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text('Provider'),
           ),
+          body: Consumer<mainModel>(builder: (_, model, child) {
+            return Center(
+              child: Column(
+                children: [
+                  const SizedBox(height: 250),
+                  Text(
+                    model.text,
+                    style: TextStyle(
+                      fontSize: 30,
+                    ),
+                  ),
+                  RaisedButton(
+                    child: Text('change'),
+                    onPressed: () {
+                      model.changeText();
+                    },
+                  ),
+                ],
+              ),
+            );
+          }),
         ),
       ),
     );
---
+++ lib/mainModel.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class mainModel extends ChangeNotifier {
+  String text = 'Provider';
+
+  void changeText() {
+    text = 'Provider Change';
+  }
+}
